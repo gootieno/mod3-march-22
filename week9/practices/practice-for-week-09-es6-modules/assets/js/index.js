10df50e05aab4ecf9bb12a4bf2c98e1119b978ac
geoffrey otieno <gotieno@geoffreys-MacBook-Pro.local>
2022-05-09 16:02:21 -0700

    solutions and practices for the day

--- week9/practices/practice-for-week-09-es6-modules/assets/js/index.js
+++ week9/practices/practice-for-week-09-es6-modules/assets/js/index.js
@@ -1,3 +1,7 @@
+//!!START SILENT
+import Game from './game.js';
+
+//!!END
 window.onload = () => {
     const game = new Game();
     game.start();
--- week9/practices/practice-for-week-09-es6-modules/assets/js/quotes/mrPotatoHead.js
+++ week9/practices/practice-for-week-09-es6-modules/assets/js/quotes/mrPotatoHead.js
@@ -1,4 +1,9 @@
-const mrPotatoHeadQuotes = {
+//!!ADD
+// const mrPotatoHeadQuotes = {
+//!!END_ADD
+//!!START SILENT
+export const mrPotatoHeadQuotes = {
+//!!END
     "hello": "Hi, I'm Mr. Potato Head!",
     "bye": "Bye, it's been nice talking to you!"
 }
