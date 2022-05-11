dcfb5823ac34a502902ce0c406a91ff8a7440b86
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2022-05-11 11:19:05 -0700

    dataset solution with domcontent loaded and window onload

--- week9/wednesday/practices/data-in-elements/starter/data.js
+++ week9/wednesday/practices/data-in-elements/starter/data.js
@@ -1,25 +1,51 @@
-window.addEventListener("DOMContentLoaded", event => {
-    console.log('domcontentloaded');
-    const shoppingList = document.getElementById("shopping-list");
-    document.getElementById("add")
-        .addEventListener("click", e => {
-            console.log('clicking');
-            e.preventDefault();
-            const name = document.getElementById("name");
-            console.log(name);
-            const type = document.getElementById("type");
-            console.log(name.value, type.value);
-            const li = document.createElement("li");
-            li.innerText = name.value;
-            li.dataset.type = type.value;
-            console.log(li);
-            // li.setAttribute("data-type", type.value);
-            shoppingList.appendChild(li);
+// const domVals = (event) => {
+//   console.log("domcontentloaded");
+//   const shoppingList = document.getElementById("shopping-list");
+//   document.getElementById("add").addEventListener("click", (e) => {
+//     console.log("clicking");
+//     e.preventDefault();
+//     const name = document.getElementById("name");
+//     console.log(name);
+//     const type = document.getElementById("type");
+//     console.log(name.value, type.value);
+//     const li = document.createElement("li");
+//     li.innerText = name.value;
+//     li.dataset.type = type.value;
+//     console.log(li);
+//     // li.setAttribute("data-type", type.value);
+//     shoppingList.appendChild(li);
 
-            if (li.dataset.type === 'drinks') {
-                li.style.backgroundColor = 'red';
-            }
-            name.value = "";
-            type.value = "drinks";
-        })
-})
+//     // if (li.dataset.type === 'drinks') {
+//     //     li.style.backgroundColor = 'red';
+//     // }
+//     name.value = "";
+//     type.value = "drinks";
+//   });
+// };
+
+// window.onload = domVals;
+
+window.document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("domcontentloaded");
+  const shoppingList = document.getElementById("shopping-list");
+  document.getElementById("add").addEventListener("click", (e) => {
+    console.log("clicking");
+    e.preventDefault();
+    const name = document.getElementById("name");
+    console.log(name);
+    const type = document.getElementById("type");
+    console.log(name.value, type.value);
+    const li = document.createElement("li");
+    li.innerText = name.value;
+    li.dataset.type = type.value;
+    console.log(li);
+    // li.setAttribute("data-type", type.value);
+    shoppingList.appendChild(li);
+
+    if (li.dataset.type === "drinks") {
+      li.style.backgroundColor = "red";
+    }
+    name.value = "";
+    type.value = "drinks";
+  });
+});
